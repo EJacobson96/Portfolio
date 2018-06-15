74b867e3e596693d584742f917d67ee7397d2caf
EJacobson96 <eric.jj96@gmail.com>
2018-06-15 07:04:21 -0700

    fix typo

--- src/components/About.js
+++ src/components/About.js
@@ -17,7 +17,7 @@ class About extends Component {
                         I recently graduated from the University of Washington with Bachelor of Science in 
                         Informatics in June 2018. My passion is creating technology that has a deep impact and improves the lives
                         of users. I have always enjoyed building new things and being able to use my analytical skills to solve
-                        complex problems. <br/> <br/> When I'm not coding, you can find me at the nearest cofee shop, working out at the gym
+                        complex problems. <br/> <br/> When I'm not coding, you can find me at the nearest coffee shop, working out at the gym
                         or hiking at one of the many beautiful hiking trails in the PNW. <br/> <br/>I'm always open to working on interesting
                         projects, feel free to reach out <br/> - <a className='contact' href='mailto:eric.jj96@gmail.com' alt='email'>eric.jj96@gmail.com</a>
                     </p>
